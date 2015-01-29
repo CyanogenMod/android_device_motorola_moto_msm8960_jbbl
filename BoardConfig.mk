92dc1f32dc6e8af3e5347778bd83586a041e88d3
moonlightly <906268147@qq.com>
2015-01-30 06:36:14 +0800

    remove TARGET_KERNEL_SELINUX_CONFIG:set by default config
    Change-Id: I38f2222072e18e64af2a65185533f5c920c3f21c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,7 +48,6 @@ BOARD_USES_LEGACY_MMAP := true
 # Inline kernel building
 TARGET_KERNEL_SOURCE := kernel/motorola/msm8960-common
 TARGET_KERNEL_CONFIG := msm8960_mmi_defconfig
-TARGET_KERNEL_SELINUX_CONFIG := msm8960_mmi_selinux_defconfig
 BOARD_KERNEL_CMDLINE := console=/dev/null androidboot.hardware=qcom user_debug=31 loglevel=1 zcache
 BOARD_KERNEL_BASE := 0x80200000
 BOARD_KERNEL_PAGESIZE := 2048
